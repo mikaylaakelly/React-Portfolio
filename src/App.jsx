15a542376c6279632bbb94e5575b8c335d48c41c
Mikayla <149916992+mikaylaakelly@users.noreply.github.com>
2024-04-14 14:42:28 -0400

    added all the information to the portfolio projects, fixed the issue about me now loads as the first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,61 @@
+// import React, { useState, useEffect } from 'react';
+// import './App.css';
+// import Header from './components/Header/Header';
+// import AboutMe from './components/aboutMe/aboutMe';
+// import Resume from './components/Resume/Resume';
+// import Portfolio from './components/Portfolio/Portfolio';
+// import ContactMe from './components/ContactMe/ContactMe';
+// import Navbar from './components/Navbar/Navbar';
+// import Footer from './components/Footer/Footer';
+
+
+
+// function App() {
+//   const [selectedSection, setSelectedSection] = useState('AboutMe'); 
+
+//   const handleNavigationClick = (section) => {
+//     setSelectedSection(section);
+//   };
+
+//   useEffect(() => {
+//   }, [selectedSection]);
+
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Navbar onNavigationClick={handleNavigationClick} />
+//       <main>
+//         {selectedSection === 'aboutMe' && <AboutMe />}
+//         {selectedSection === 'resume' && <Resume />}
+//         {selectedSection === 'portfolio' && <Portfolio />}
+//         {selectedSection === 'contactMe' && <ContactMe />}
+//       </main>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import AboutMe from './components/aboutMe/aboutMe';
+import AboutMe from './components/AboutMe/AboutMe';
 import Resume from './components/Resume/Resume';
 import Portfolio from './components/Portfolio/Portfolio';
 import ContactMe from './components/ContactMe/ContactMe';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 
-
-
 function App() {
-  const [selectedSection, setSelectedSection] = useState('AboutMe'); 
+  const [selectedSection, setSelectedSection] = useState('aboutMe'); // Set initial value to 'aboutMe'
 
   const handleNavigationClick = (section) => {
     setSelectedSection(section);
   };
 
   useEffect(() => {
+    
   }, [selectedSection]);
 
   return (
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,24 +1,51 @@
 import React from 'react';
 
-function Portfolio() {
-  const projects = [
+const projects = [
     {
-      id: 1,
-      title: 'Project 1',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel velit nec sem suscipit tristique.'
+        id: 1,
+        title: 'Weather App',
+        description: 'An app that you can use to search for the weather in a city and be provided with that day\'s weather and a 5-day forecast.',
+        githubLink: 'https://github.com/mikaylaakelly/module-6-weatherAPI',
+        deployedLink: 'https://mikaylaakelly.github.io/module-6-weatherAPI/'
     },
-    {
-      id: 2,
-      title: 'Project 2',
-      description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
-    },
-    {
-      id: 3,
-      title: 'Project 3',
-      description: 'Fusce id orci convallis, maximus mi nec, ultricies ipsum. Nullam hendrerit lorem ut ex placerat.'
-    }
-  ];
+  {
+    id: 2,
+    title: 'Work Day Scheduler',
+    description: 'A simple calendar app for scheduling your work day',
+    githubLink: 'https://github.com/mikaylaakelly/module-5-calendar',
+    deployedLink: 'https://mikaylaakelly.github.io/module-5-calendar/'
+  },
+  {
+    id: 3,
+    title: 'ReadMe Generator',
+    description: 'Fusce id orci convallis, maximus mi nec, ultricies ipsum. Nullam hendrerit lorem ut ex placerat.',
+    githubLink: 'https://github.com/mikaylaakelly/ReadMeGenerator',
+    deployedLink: 'https://drive.google.com/file/d/1LspdURNtchZHVwCS5sCkwSPtpY9nlRaH/view'
+  },
+  {
+    id: 4,
+    title: 'Quiz App',
+    description: 'A fun quiz app to quiz you on your beginner knowledge of coding.',
+    githubLink: 'https://github.com/mikaylaakelly/module-4-quiz',
+    deployedLink: 'https://mikaylaakelly.github.io/module-4-quiz/'
+  },
+  {
+    id: 5,
+    title: 'Recipe Place',
+    description: 'A group project where we created an app that can search for recipes and ingredients that you have on hand, and returns a list of recipes you can make. You can then save the recipes you like to use later',
+    githubLink: 'https://github.com/Ashloraptor/MARJ',
+    deployedLink: 'https://ashloraptor.github.io/MARJ/'
+  },
+  {
+    id: 6,
+    title: 'Compendium',
+    description: 'A plant identifying app where you can create a profile and then use the user input and plant.id API to pull up information on plants that you can save to your profile.',
+    githubLink: 'https://github.com/Ashloraptor/Compendium',
+    deployedLink: 'https://compendium-256a42904ce0.herokuapp.com/'
+  }
+];
 
+function Portfolio() {
   return (
     <div className="portfolio">
       <h2>Portfolio</h2>
@@ -27,6 +54,9 @@ function Portfolio() {
           <div className="project" key={project.id}>
             <h3>{project.title}</h3>
             <p>{project.description}</p>
+            <a href={project.githubLink}>Github Repo</a>
+            <br />
+            <a href={project.deployedLink}>Deployed Link/Video Walkthrough</a>
           </div>
         ))}
       </div>
@@ -35,6 +65,7 @@ function Portfolio() {
 }
 
 export default Portfolio;
+// export default Portfolio;
 
 // This is from activity 24/solved/components/pages/aboutpage THIS IS HOW I WANT THE PORTFOLIO PAGE TO LOOK
 // export default function AboutPage() {
