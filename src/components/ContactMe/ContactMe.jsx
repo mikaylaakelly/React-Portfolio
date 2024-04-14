5fa3afe19b8c1d4df3ad51444b2bc30ac4fba6cd
Mikayla <149916992+mikaylaakelly@users.noreply.github.com>
2024-04-14 13:03:10 -0400

    changed helpers.js function into a variable and moved into contactme.jsx. less files the better

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -75,3 +75,85 @@ function ContactMe() {
 }
 
 export default ContactMe;
+
+// import { useState } from 'react';
+// import './ContactMe.css';
+// import { checkPassword, validateEmail } from './helpers';
+
+// function Form() {
+//   const [email, setEmail] = useState('');
+//   const [userName, setUserName] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [errorMessage, setErrorMessage] = useState('');
+
+//   const handleInputChange = (e) => {
+//     const { target } = e;
+//     const inputType = target.name;
+//     const inputValue = target.value;
+
+//     if (inputType === 'email') {
+//       setEmail(inputValue);
+//     } else if (inputType === 'userName') {
+//       setUserName(inputValue);
+//     } else {
+//       setPassword(inputValue);
+//     }
+//   };
+
+//   const handleFormSubmit = (e) => {
+//     e.preventDefault();
+
+//     if (!validateEmail(email) || !userName) {
+//       setErrorMessage('Email or username is invalid');
+//       return;
+//     }
+//     if (!checkPassword(password)) {
+//       setErrorMessage(
+//         `Choose a more secure password for the account: ${userName}`
+//       );
+//       return;
+//     }
+//     alert(`Hello ${userName}`);
+
+//     setUserName('');
+//     setPassword('');
+//     setEmail('');
+//   };
+
+//   return (
+//     <div className="container text-center">
+//       <h1>Hello {userName}</h1>
+//       <form className="form" onSubmit={handleFormSubmit}>
+//         <input
+//           value={email}
+//           name="email"
+//           onChange={handleInputChange}
+//           type="email"
+//           placeholder="Email"
+//         />
+//         <input
+//           value={userName}
+//           name="userName"
+//           onChange={handleInputChange}
+//           type="text"
+//           placeholder="Username"
+//         />
+//         <input
+//           value={password}
+//           name="password"
+//           onChange={handleInputChange}
+//           type="password"
+//           placeholder="Password"
+//         />
+//         <button type="submit">Submit</button>
+//       </form>
+//       {errorMessage && (
+//         <div>
+//           <p className="error-text">{errorMessage}</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Form;
